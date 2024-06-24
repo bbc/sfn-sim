01444b701628d3f6951d1dbf1a48ea9c5933eab7
Ben-Wormald <benwormald95@gmail.com>
2024-06-24 11:20:19 +0100

    add IsTimestamp, StringMatches choice expressions, choice tests

--- src/choice.js
+++ src/choice.js
@@ -1,4 +1,4 @@
-import { NoChoiceMatchedError, RuntimeError, SimulatorError } from './errors.js';
+import { NoChoiceMatchedError, RuntimeError } from './errors.js';
 import { getValue } from './utils.js';
 
 const runChoice = (state, _context, input) => {
@@ -19,74 +19,118 @@ const evaluateChoiceRule = (choice, input) => {
   if (choice.Not) {
     return !evaluateChoiceRule(choice.Not, input);
   }
+
   if (choice.Or) {
     return choice.Or.some((subChoice) => evaluateChoiceRule(subChoice, input));
   }
+
   if (choice.And) {
     return choice.And.every((subChoice) => evaluateChoiceRule(subChoice, input));
   }
+
   if (choice.StringEquals || choice.NumericEquals || choice.BooleanEquals || choice.TimestampEquals) {
     const operand = choice.StringEquals || choice.NumericEquals || choice.BooleanEquals || choice.TimestampEquals;
     return getValue(input, choice.Variable) === operand;
   }
+
   if (choice.StringEqualsPath || choice.NumericEqualsPath || choice.BooleanEqualsPath || choice.TimestampEqualsPath) {
     const operandPath = choice.StringEqualsPath || choice.NumericEqualsPath || choice.BooleanEqualsPath || choice.TimestampEqualsPath;
     return getValue(input, choice.Variable) === getValue(input, operandPath);
   }
+
   if (choice.StringLessThan || choice.NumericLessThan || choice.TimestampLessThan) {
     const operand = choice.StringLessThan || choice.NumericLessThan || choice.TimestampLessThan;
     return getValue(input, choice.Variable) < operand;
   }
+
   if (choice.StringLessThanPath || choice.NumericLessThanPath || choice.TimestampLessThanPath) {
     const operandPath = choice.StringLessThanPath || choice.NumericLessThanPath || choice.TimestampLessThanPath;
     return getValue(input, choice.Variable) < getValue(input, operandPath);
   }
+
   if (choice.StringLessThanEqual || choice.NumericLessThanEqual || choice.TimestampLessThanEqual) {
     const operand = choice.StringLessThanEqual || choice.NumericLessThanEqual || choice.TimestampLessThanEqual;
     return getValue(input, choice.Variable) <= operand;
   }
+
   if (choice.StringLessThanEqualPath || choice.NumericLessThanEqualPath || choice.TimestampLessThanEqualPath) {
     const operandPath = choice.StringLessThanEqualPath || choice.NumericLessThanEqualPath || choice.TimestampLessThanEqualPath;
     return getValue(input, choice.Variable) <= getValue(input, operandPath);
   }
+
   if (choice.StringGreaterThan || choice.NumericGreaterThan || choice.TimestampGreaterThan) {
     const operand = choice.StringGreaterThan || choice.NumericGreaterThan || choice.TimestampGreaterThan;
     return getValue(input, choice.Variable) > operand;
   }
+
   if (choice.StringGreaterThanPath || choice.NumericGreaterThanPath || choice.TimestampGreaterThanPath) {
     const operandPath = choice.StringGreaterThanPath || choice.NumericGreaterThanPath || choice.TimestampGreaterThanPath;
     return getValue(input, choice.Variable) > getValue(input, operandPath);
   }
+
   if (choice.StringGreaterThanEqual || choice.NumericGreaterThanEqual || choice.TimestampGreaterThanEqual) {
     const operand = choice.StringGreaterThanEqual || choice.NumericGreaterThanEqual || choice.TimestampGreaterThanEqual;
     return getValue(input, choice.Variable) >= operand;
   }
+
   if (choice.StringGreaterThanEqualPath || choice.NumericGreaterThanEqualPath || choice.TimestampGreaterThanEqualPath) {
     const operandPath = choice.StringGreaterThanEqualPath || choice.NumericGreaterThanEqualPath || choice.TimestampGreaterThanEqualPath;
     return getValue(input, choice.Variable) >= getValue(input, operandPath);
   }
-  if (choice.IsNull) {
-    return getValue(input, choice.Variable) === null;
+
+  if (choice.IsNull !== undefined) {
+    return (getValue(input, choice.Variable) === null) === choice.IsNull;
   }
+
   if (choice.IsPresent !== undefined) {
     return !!getValue(input, choice.Variable) === choice.IsPresent;
   }
-  if (choice.IsNumeric) {
-    return typeof getValue(input, choice.Variable) === 'number';
+
+  if (choice.IsNumeric !== undefined) {
+    return (typeof getValue(input, choice.Variable) === 'number') === choice.IsNumeric;
   }
-  if (choice.IsString) {
-    return typeof getValue(input, choice.Variable) === 'string';
+
+  if (choice.IsString !== undefined) {
+    return (typeof getValue(input, choice.Variable) === 'string') === choice.IsString;
   }
-  if (choice.IsBoolean) {
-    return typeof getValue(input, choice.Variable) === 'boolean';
+
+  if (choice.IsBoolean !== undefined) {
+    return (typeof getValue(input, choice.Variable) === 'boolean') === choice.IsBoolean;
   }
-  if (choice.IsTimestamp) {
-    throw new SimulatorError('IsTimestamp data-test expression unimplemented');
+
+  if (choice.IsTimestamp !== undefined) {
+    const timestamp = new RegExp(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d{3})?(Z|((\+|-)\d{2}:\d{2}))$/);
+    return timestamp.test(getValue(input, choice.Variable)) === choice.IsTimestamp;
   }
+
   if (choice.StringMatches) {
-    throw new SimulatorError('StringMatches data-test expression unimplemented');
+    const matcher = choice.StringMatches;
+    const string = getValue(input, choice.Variable);
+
+    const parts = matcher
+      .replace(/\\\*/g, 'ESCAPED_ASTERISK')
+      .split('*')
+      .map((part) => part.replace(/ESCAPED_ASTERISK/g, '\*'));
+
+    let valid = true;
+    let previousIndex = 0;
+
+    for (const part of parts) {
+      if (!(string.indexOf(part) >= previousIndex)) {
+        valid = false;
+      }
+
+      previousIndex = string.indexOf(part) + part.length;
+    }
+
+    return valid;
   }
+
   throw new RuntimeError('Choice does not contain a data-test expression');
 };
 
 export default runChoice;
+
+export {
+  evaluateChoiceRule,
+};
--- tests/choice.test.js
+++ tests/choice.test.js
@@ -1,66 +1,400 @@
-import { test, expect } from 'vitest';
-import runChoice from '../src/choice.js';
+import { test, expect, describe } from 'vitest';
+import runChoice, { evaluateChoiceRule } from '../src/choice.js';
 import { NoChoiceMatchedError } from '../src/errors.js';
 
-test('returns a matched step from the given Choices', () => {
-  const state = {
-    Type: 'Choice',
-    Choices: [
-      {
-        Variable: '$.someString',
-        StringEquals: 'Hello!',
-        Next: 'MatchedStep',
-      },
-    ],
-    Default: 'DefaultStep',
-  };
+describe('runChoice', () => {
+  test('returns a matched step from the given Choices', () => {
+    const state = {
+      Type: 'Choice',
+      Choices: [
+        {
+          Variable: '$.someString',
+          StringEquals: 'Hello!',
+          Next: 'MatchedStep',
+        },
+      ],
+      Default: 'DefaultStep',
+    };
 
-  const input = {
-    someString: 'Hello!',
-  };
+    const input = {
+      someString: 'Hello!',
+    };
 
-  const nextState = runChoice(state, {}, input);
+    const nextState = runChoice(state, {}, input);
 
-  expect(nextState).toEqual('MatchedStep');
+    expect(nextState).toEqual('MatchedStep');
+  });
+
+  test('returns the Default step when no choices match', () => {
+    const state = {
+      Type: 'Choice',
+      Choices: [
+        {
+          Variable: '$.someString',
+          StringEquals: 'Hello!',
+          Next: 'MatchedStep',
+        },
+      ],
+      Default: 'DefaultStep',
+    };
+
+    const input = {
+      someString: 'Goodbye!',
+    };
+
+    const nextState = runChoice(state, {}, input);
+
+    expect(nextState).toEqual('DefaultStep');
+  });
+
+  test('throws a States.NoChoiceMatched error when no choices match and no Default is specified', () => {
+    const state = {
+      Type: 'Choice',
+      Choices: [
+        {
+          Variable: '$.someString',
+          StringEquals: 'Hello!',
+          Next: 'MatchedStep',
+        },
+      ],
+    };
+
+    const input = {
+      someString: 'Goodbye!',
+    };
+
+    expect(() => runChoice(state, {}, input)).toThrowError(NoChoiceMatchedError);
+  });
 });
 
-test('returns the Default step when no choices match', () => {
-  const state = {
-    Type: 'Choice',
-    Choices: [
-      {
-        Variable: '$.someString',
-        StringEquals: 'Hello!',
-        Next: 'MatchedStep',
+describe('evaluateChoiceRule', () => {
+  describe('Not', () => {
+    const choice = {
+      Not: {
+        BooleanEquals: true,
+        Variable: '$.myBoolean',
       },
-    ],
-    Default: 'DefaultStep',
-  };
+    };
 
-  const input = {
-    someString: 'Goodbye!',
-  };
+    test('returns true', () => {
+      const result = evaluateChoiceRule(choice, { myBoolean: false });
+      expect(result).toBe(true);
+    });
 
-  const nextState = runChoice(state, {}, input);
+    test('returns false', () => {
+      const result = evaluateChoiceRule(choice, { myBoolean: true });
+      expect(result).toBe(false);
+    });
+  });
 
-  expect(nextState).toEqual('DefaultStep');
-});
+  describe('Or', () => {
+    const choice = {
+      Or: [
+        {
+          BooleanEquals: true,
+          Variable: '$.myBooleanA',
+        },
+        {
+          BooleanEquals: true,
+          Variable: '$.myBooleanB',
+        },
+      ],
+    };
 
-test('throws a States.NoChoiceMatched error when no choices match and no Default is specified', () => {
-  const state = {
-    Type: 'Choice',
-    Choices: [
-      {
-        Variable: '$.someString',
-        StringEquals: 'Hello!',
-        Next: 'MatchedStep',
-      },
-    ],
-  };
+    test('returns true', () => {
+      const result = evaluateChoiceRule(choice, { myBooleanA: false, myBooleanB: true });
+      expect(result).toBe(true);
+    });
 
-  const input = {
-    someString: 'Goodbye!',
-  };
+    test('returns false', () => {
+      const result = evaluateChoiceRule(choice, { myBooleanA: false, myBooleanB: false });
+      expect(result).toBe(false);
+    });
+  });
 
-  expect(() => runChoice(state, {}, input)).toThrowError(NoChoiceMatchedError);
+  describe('And', () => {
+    const choice = {
+      And: [
+        {
+          BooleanEquals: true,
+          Variable: '$.myBooleanA',
+        },
+        {
+          BooleanEquals: true,
+          Variable: '$.myBooleanB',
+        },
+      ],
+    };
+
+    test('returns true', () => {
+      const result = evaluateChoiceRule(choice, { myBooleanA: true, myBooleanB: true });
+      expect(result).toBe(true);
+    });
+
+    test('returns false', () => {
+      const result = evaluateChoiceRule(choice, { myBooleanA: true, myBooleanB: false });
+      expect(result).toBe(false);
+    });
+  });
+
+  describe('StringEquals', () => {
+    const choice = {
+      StringEquals: 'myValue',
+      Variable: '$.myString',
+    };
+
+    test('returns true', () => {
+      const result = evaluateChoiceRule(choice, { myString: 'myValue' });
+      expect(result).toBe(true);
+    });
+
+    test('returns false', () => {
+      const result = evaluateChoiceRule(choice, { myString: 'someOtherValue' });
+      expect(result).toBe(false);
+    });
+  });
+
+  describe('NumericEquals', () => {
+    const choice = {
+      NumericEquals: 2,
+      Variable: '$.myNumber',
+    };
+
+    test('returns true', () => {
+      const result = evaluateChoiceRule(choice, { myNumber: 2 });
+      expect(result).toBe(true);
+    });
+
+    test('returns false', () => {
+      const result = evaluateChoiceRule(choice,{ myNumber: 3 });
+      expect(result).toBe(false);
+    });
+  });
+
+  describe('BooleanEquals', () => {
+    const choice = {
+      BooleanEquals: true,
+      Variable: '$.myBoolean',
+    };
+
+    test('returns true', () => {
+      const result = evaluateChoiceRule(choice, { myBoolean: true });
+      expect(result).toBe(true);
+    });
+
+    test('returns false', () => {
+      const result = evaluateChoiceRule(choice,{ myBoolean: false });
+      expect(result).toBe(false);
+    });
+  });
+
+  describe('TimestampEquals', () => {
+    const date = new Date();
+    const choice = {
+      TimestampEquals: date.toISOString(),
+      Variable: '$.myTimestamp',
+    };
+
+    test('returns true', () => {
+      const result = evaluateChoiceRule(choice, { myTimestamp: date.toISOString() });
+      expect(result).toBe(true);
+    });
+
+    test('returns false', () => {
+      const otherTimestamp = new Date(new Date().setDate(date.getDate() - 5)).toISOString();
+      const result = evaluateChoiceRule(choice,{ myTimestamp: otherTimestamp });
+      expect(result).toBe(false);
+    });
+  });
+
+  describe('StringEqualsPath', () => {
+    const choice = {
+      StringEqualsPath: '$.myStringA',
+      Variable: '$.myStringB',
+    };
+
+    test('returns true', () => {
+      const result = evaluateChoiceRule(choice, { myStringA: 'myValue', myStringB: 'myValue' });
+      expect(result).toBe(true);
+    });
+
+    test('returns false', () => {
+      const result = evaluateChoiceRule(choice, { myStringA: 'myValue', myStringB: 'someOtherValue' });
+      expect(result).toBe(false);
+    });
+  });
+
+  describe('NumericEqualsPath', () => {
+    const choice = {
+      NumericEqualsPath:'$.myNumberA',
+      Variable: '$.myNumberB',
+    };
+
+    test('returns true', () => {
+      const result = evaluateChoiceRule(choice, { myNumberA: 2, myNumberB: 2 });
+      expect(result).toBe(true);
+    });
+
+    test('returns false', () => {
+      const result = evaluateChoiceRule(choice,{ myNumberA: 2, myNumberB: 3 });
+      expect(result).toBe(false);
+    });
+  });
+
+  describe('BooleanEqualsPath', () => {
+    const choice = {
+      BooleanEqualsPath: '$.myBooleanA',
+      Variable: '$.myBooleanB',
+    };
+
+    test('returns true', () => {
+      const result = evaluateChoiceRule(choice, { myBooleanA: true, myBooleanB: true });
+      expect(result).toBe(true);
+    });
+
+    test('returns false', () => {
+      const result = evaluateChoiceRule(choice,{ myBooleanA: true, myBooleanB: false });
+      expect(result).toBe(false);
+    });
+  });
+
+  describe('TimestampEqualsPath', () => {
+    const choice = {
+      TimestampEqualsPath: '$.myTimestampA',
+      Variable: '$.myTimestampB',
+    };
+
+    test('returns true', () => {
+      const timestamp = new Date().toISOString();
+      const result = evaluateChoiceRule(choice, { myTimestampA: timestamp, myTimestampB: timestamp });
+      expect(result).toBe(true);
+    });
+
+    test('returns false', () => {
+      const date = new Date();
+      const myTimestampA = date.toISOString();
+      const myTimestampB = new Date(new Date().setDate(date.getDate() - 5)).toISOString();
+      const result = evaluateChoiceRule(choice,{ myTimestampA, myTimestampB });
+      expect(result).toBe(false);
+    });
+  });
+
+  describe('IsNull', () => {
+    const choice = {
+      IsNull: true,
+      Variable: '$.myVariable',
+    };
+
+    test('returns true', () => {
+      const result = evaluateChoiceRule(choice, { myVariable: null });
+      expect(result).toBe(true);
+    });
+
+    test('returns false', () => {
+      const result = evaluateChoiceRule(choice,{ myVariable: 'someValue' });
+      expect(result).toBe(false);
+    });
+  });
+
+  describe('IsPresent', () => {
+    const choice = {
+      IsPresent: true,
+      Variable: '$.myVariable',
+    };
+
+    test('returns true', () => {
+      const result = evaluateChoiceRule(choice, { myVariable: 'someValue' });
+      expect(result).toBe(true);
+    });
+
+    test('returns false', () => {
+      const result = evaluateChoiceRule(choice,{ someOtherVariable: 'someValue' });
+      expect(result).toBe(false);
+    });
+  });
+
+  describe('IsNumeric', () => {
+    const choice = {
+      IsNumeric: true,
+      Variable: '$.myVariable',
+    };
+
+    test('returns true', () => {
+      const result = evaluateChoiceRule(choice, { myVariable: 2 });
+      expect(result).toBe(true);
+    });
+
+    test('returns false', () => {
+      const result = evaluateChoiceRule(choice,{ myVariable: 'two' });
+      expect(result).toBe(false);
+    });
+  });
+
+  describe('IsString', () => {
+    const choice = {
+      IsString: true,
+      Variable: '$.myVariable',
+    };
+
+    test('returns true', () => {
+      const result = evaluateChoiceRule(choice, { myVariable: 'hello' });
+      expect(result).toBe(true);
+    });
+
+    test('returns false', () => {
+      const result = evaluateChoiceRule(choice,{ myVariable: 42 });
+      expect(result).toBe(false);
+    });
+  });
+
+  describe('IsBoolean', () => {
+    const choice = {
+      IsBoolean: true,
+      Variable: '$.myVariable',
+    };
+
+    test('returns true', () => {
+      const result = evaluateChoiceRule(choice, { myVariable: false });
+      expect(result).toBe(true);
+    });
+
+    test('returns false', () => {
+      const result = evaluateChoiceRule(choice,{ myVariable: 'TRUE' });
+      expect(result).toBe(false);
+    });
+  });
+
+  describe('IsTimestamp', () => {
+    const choice = {
+      IsTimestamp: true,
+      Variable: '$.myVariable',
+    };
+
+    test('returns true', () => {
+      const result = evaluateChoiceRule(choice, { myVariable: new Date().toISOString() });
+      expect(result).toBe(true);
+    });
+
+    test('returns false', () => {
+      const result = evaluateChoiceRule(choice,{ myVariable: '24th June 2024' });
+      expect(result).toBe(false);
+    });
+  });
+
+  describe('StringMatches', () => {
+    const choice = {
+      StringMatches: '*.test.js',
+      Variable: '$.myString',
+    };
+
+    test('returns true', () => {
+      const result = evaluateChoiceRule(choice, { myString: 'tests/choice.test.js' });
+      expect(result).toBe(true);
+    });
+
+    test('returns false', () => {
+      const result = evaluateChoiceRule(choice,{ myString: 'src/choice.js' });
+      expect(result).toBe(false);
+    });
+  });
 });
